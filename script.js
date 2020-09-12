06e3cd0bb1eb6ff5d0a30cc8037e233e94c9e587
Chris MacGeorge <christopher.s.macgeorge@gmail.com>
2020-09-11 19:03:29 -0700

    script for pw generator

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Assignment Code
+// Assignment Code (given)
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input (given)
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,20 +10,113 @@ function writePassword() {
 
 }
 
-// TODO create generate password function that returns the fianl password and all of my code should live inside this fucntion
+// VAR Arrays
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var letterUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var letterLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specCharacter = ["!", "%", "&", "$", "+", "-", "?", "~", "<", ">", "_"]; 
+var random = Math.floor(Math.random());
 
-// TODO create promt to ask the length of the password and save it as a VAR (prompt and variable )
+// Starting the function of the button
+function generatePassword() {
+  var finalPassword = ""
+  confirmLength = prompt("How many characters do you want your PW to be? Must be b/t 8 and 128");
+  console.log("Password length " + confirmLength)
 
-// TODO creat a Confirm to ask user if they want lowercase characters
+  if(!confirmLength) {
+    alert("Must Enter Appropriate Value!");
+  } 
+  // If user does not enter correct criteria
+  else if (confirmLength < 8 || confirmLength > 128) {
+    confirmLength = prompt ("Choose b/t 8 and 128");
+    console.log("Password length " + confirmLength);
+  } 
+  // Prompts
+  else {
+    confirmNumber = confirm("Would you like to include numbers?");
+    console.log("Number" + confirmNumber)
+    confirmUppercase = confirm("would you like this to inclue capital letters?");
+    console.log("Uppercase" + confirmUppercase)
+    confirmLowercase = confirm("Would you like to include lowercase letters?");
+    console.log("Lowercase" + confirmLowercase)
+    confirmSpecChar = confirm("Would you like to include special characters?");
+    console.log("Special Character" + confirmSpecChar)
+  };
+  // False answers
+  if (!confirmNumber && !confirmUppercase && !confirmLowercase && !confirmSpecChar) {
+    userChoice = alert("Choose criteria");
+  } 
+  // True answers
+  else if (confirmNumber && confirmUppercase && confirmLowercase && confirmSpecChar) {
+    userChoice = number.concat(letterUppercase, letterLowercase, specCharacter);
+    console.log(userChoice);
+  }
+  else if (confirmNumber && confirmUppercase && confirmLowercase) {
+    userChoice = number.concat(letterUppercase, letterLowercase);
+    console.log(userChoice);
+  }
+  else if (confirmNumber && confirmUppercase && confirmSpecChar) {
+    userChoice = number.concat(letterUppercase, specCharacter);
+    console.log(userChoice);
+  }
+  else if (confirmNumber && confirmLowercase && confirmSpecChar) {
+    userChoice = number.concat(letterLowercase, specCharacter);
+    console.log(userChoice);
+  }
+  else if (confirmUppercase && confirmLowercase && confirmSpecChar) {
+    userChoice = letterUppercase.concat(letterLowercase, specCharacter);
+    console.log(userChoice);
+  }
+  else if (confirmNumber && confirmUppercase) {
+    userChoice = number.concat(letterUppercase)
+    console.log(userChoice);
+  }
+  else if (confirmNumber && confirmLowercase) {
+    userChoice = number.concat(letterLowercase)
+    console.log(userChoice);
+  }
+  else if (confirmNumber && confirmSpecChar) {
+    userChoice = number.concat(specCharacter);
+    console.log(userChoice);
+  }
+  else if (confirmUppercase && confirmLowercase) {
+    userChoice = letterUppercase.concat(letterLowercase);
+    console.log(userChoice);
+  }
+  else if (confirmUppercase && confirmSpecChar) {
+    userChoice = letterUppercase.concat(specCharacter);
+    console.log(userChoice);
+  }
+  else if (confirmLowercase && confirmSpecChar) {
+    userChoice = letterLowercase.concat(specCharacter);
+    console.log(userChoice);
+  }
+  else if (confirmNumber) {
+    userChoice = number;
+    console.log(userChoice);
+  }
+  else if (confirmUppercase) {
+    userChoice = letterUppercase;
+    console.log(userChoice);
+  }
+  else if (confirmLowercase) {
+    userChoice = letterLowercase;
+    console.log(userChoice);
+  }
+  else if (confirmSpecChar) {
+    userChoice = specCharacter;
+    console.log(userChoice);
+  };
 
-// TODO creat a Confirm to ask user if they want uppercase characters
-
-// TODO creat a Confirm to ask user if they want numbers characters
-
-// TODO creat a Confirm to ask user if they want special characters
-
-// Add validations to make sure at least one type of character is slelected
-
+  for (var i = 0; i < confirmLength; i++) {
+    var allChoices = userChoice[Math.floor(Math.random() * userChoice.confirmLength)];
+    finalPassword = finalPassword + allChoices;
+    console.log(allChoices)
+  }
+  
+  console.log("Password: " + finalPassword);
+  return finalPassword;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
